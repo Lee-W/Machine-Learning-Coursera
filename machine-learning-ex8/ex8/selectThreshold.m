836a247643f08c8e3c3e9076a6ba003c21dec3f5
Lee-W <cl87654321@gmail.com>
2016-03-01 10:29:25 +0800

    Finish Gaussian

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -22,18 +22,13 @@ for epsilon = min(pval):stepsize:max(pval)
     %               
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
-
-
-
-
-
-
-
-
-
-
-
-
+    cvPrediction = pval < epsilon;
+    tp = sum((cvPrediction == 1) & (yval == 1));
+    fp = sum((cvPrediction == 1) & (yval == 0));
+    fn = sum((cvPrediction == 0) & (yval == 1));
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
+    F1 = (2 * precision * recall) / (precision + recall);
 
     % =============================================================
 
